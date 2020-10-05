fa3aa66813ece4fed96c26169ea9f26662ab3360
Aiden Jeffrey <aiden.jeffrey@codethink.co.uk>
2020-10-05 14:23:08 +0100

    Add basic Socket API (built on MBox.h)
    Derived from azure sphere IntercoreComms sample

--- IntercoreComms_Mailbox/IntercoreComms_RTApp_MT3620_BareMetal/main.c
+++ IntercoreComms_Mailbox/IntercoreComms_RTApp_MT3620_BareMetal/main.c
@@ -1,4 +1,5 @@
 /* Copyright (c) Microsoft Corporation. All rights reserved.
+   Copyright (c) Codethink Ltd. All rights reserved.
    Licensed under the MIT License. */
 
 // This sample C application for the real-time core demonstrates intercore communications by
@@ -10,223 +11,266 @@
 // - mailbox (used to report buffer sizes and send / receive events)
 // - timer (used to send a message to the HLApp)
 
-#include <ctype.h>
+/* Copyright (c) Codethink Ltd. All rights reserved.
+   Licensed under the MIT License. */
+
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <errno.h>
 
-#include "logical-dpc.h"
-#include "logical-intercore.h"
+#include "lib/mt3620/gpt.h"
+#include "lib/CPUFreq.h"
+#include "lib/VectorTable.h"
+#include "lib/NVIC.h"
+#include "lib/GPIO.h"
+#include "lib/UART.h"
+#include "lib/Print.h"
+#include "lib/GPT.h"
 
-#include "mt3620-baremetal.h"
-#include "mt3620-uart-poll.h"
-#include "mt3620-intercore.h"
-#include "mt3620-timer.h"
+#include "Socket.h"
 
-extern uint32_t StackTop; // &StackTop == end of TCM
+#define NUM_BUTTONS 2
 
-static IntercoreComm icc;
+typedef enum {
+    TIMER_BUTTONS,
+    TIMER_SEND_MSG,
+    TIMER_COUNT
+} appTimers;
 
-static const uint32_t sendTimerIntervalMs = 1000;
+// Drivers
+static UART   *debug              = NULL;
+static GPT    *timer[TIMER_COUNT] = {NULL};
 
-static _Noreturn void DefaultExceptionHandler(void);
-static void HandleSendTimerIrq(void);
-static void HandleSendTimerDeferred(void);
+static Socket *socket             = NULL;
 
-static void PrintBytes(const void *buf, int start, int end);
-static void PrintGuid(const ComponentId *cid);
+static unsigned gpioOut[2] = {0, 1};
 
-static _Noreturn void RTCoreMain(void);
+static volatile unsigned msgCounter = 0;
 
-// ARM DDI0403E.d SB1.5.2-3
-// From SB1.5.3, "The Vector table must be naturally aligned to a power of two whose alignment
-// value is greater than or equal to (Number of Exceptions supported x 4), with a minimum alignment
-// of 128 bytes.". The array is aligned in linker.ld, using the dedicated section ".vector_table".
+// Callbacks
+typedef struct CallbackNode {
+    bool enqueued;
+    struct CallbackNode *next;
+    void *data;
+    void (*cb)(void*);
+} CallbackNode;
 
-// The exception vector table contains a stack pointer, 15 exception handlers, and an entry for
-// each interrupt.
-#define INTERRUPT_COUNT 100 // from datasheet
-#define EXCEPTION_COUNT (16 + INTERRUPT_COUNT)
-#define INT_TO_EXC(i_) (16 + (i_))
-const uintptr_t ExceptionVectorTable[EXCEPTION_COUNT] __attribute__((section(".vector_table")))
-__attribute__((used)) = {
-    [0] = (uintptr_t)&StackTop,                // Main Stack Pointer (MSP)
-    [1] = (uintptr_t)RTCoreMain,               // Reset
-    [2] = (uintptr_t)DefaultExceptionHandler,  // NMI
-    [3] = (uintptr_t)DefaultExceptionHandler,  // HardFault
-    [4] = (uintptr_t)DefaultExceptionHandler,  // MPU Fault
-    [5] = (uintptr_t)DefaultExceptionHandler,  // Bus Fault
-    [6] = (uintptr_t)DefaultExceptionHandler,  // Usage Fault
-    [11] = (uintptr_t)DefaultExceptionHandler, // SVCall
-    [12] = (uintptr_t)DefaultExceptionHandler, // Debug monitor
-    [14] = (uintptr_t)DefaultExceptionHandler, // PendSV
-    [15] = (uintptr_t)DefaultExceptionHandler, // SysTick
+static void EnqueueCallback(CallbackNode *node);
 
-    [INT_TO_EXC(0)] = (uintptr_t)DefaultExceptionHandler,
-    [INT_TO_EXC(1)] = (uintptr_t)MT3620_Gpt_HandleIrq1,
-    [INT_TO_EXC(2)... INT_TO_EXC(10)] = (uintptr_t)DefaultExceptionHandler,
-    [INT_TO_EXC(11)] = (uintptr_t)MT3620_HandleMailboxIrq11,
-    [INT_TO_EXC(12)... INT_TO_EXC(INTERRUPT_COUNT - 1)] = (uintptr_t)DefaultExceptionHandler};
-
-// If the applications end up in this function then an unexpected exception has occurred.
-static _Noreturn void DefaultExceptionHandler(void)
+// Msg callbacks
+// Prints an array of bytes
+static void printBytes(const uint8_t *bytes, uintptr_t start, uintptr_t size)
 {
-    for (;;) {
-        // empty.
+    for (unsigned i = start; i < size; i++) {
+        UART_Printf(debug, "%x", bytes[i]);
     }
 }
 
-// Runs in IRQ context and schedules HandleSendTimerDeferred to run later.
-static void HandleSendTimerIrq(void)
+static void printComponentId(const Component_Id *compId)
 {
-    static CallbackNode cbn = {.enqueued = false, .cb = HandleSendTimerDeferred};
-    EnqueueDeferredProc(&cbn);
+    UART_Printf(debug, "%lx-%x-%x", compId->seg_0, compId->seg_1, compId->seg_2);
+    UART_Print(debug, "-");
+    printBytes(compId->seg_3_4, 0, 2);
+    UART_Print(debug, "-");
+    printBytes(compId->seg_3_4, 2, 8);
+    UART_Print(debug, "\r\n");
 }
 
-// Queued by HandleSendTimerIrq. Sends a message to the HLApp.
-static void HandleSendTimerDeferred(void)
+static void handleSendMsgTimer(void* data)
 {
-    static int iter = 0;
-    // The component ID for IntercoreComms_HighLevelApp.
-    static const ComponentId hlAppId = {.data1 = 0x25025d2c,
-                                        .data2 = 0x66da,
-                                        .data3 = 0x4448,
-                                        .data4 = {0xba, 0xe1, 0xac, 0x26, 0xfc, 0xdd, 0x36, 0x27}};
+    static const Component_Id A7ID =
+    {
+        .seg_0   = 0x25025d2c,
+        .seg_1   = 0x66da,
+        .seg_2   = 0x4448,
+        .seg_3_4 = {0xba, 0xe1, 0xac, 0x26, 0xfc, 0xdd, 0x36, 0x27}
+    };
 
-    // The number cycles from "00" to "99".
-    static char txMsg[] = "rt-app-to-hl-app-00";
-    const size_t txMsgLen = sizeof(txMsg);
+    static char msg[] = "rt-app-to-hl-app-00";
+    const uintptr_t msgLen = sizeof(msg);
 
-    IntercoreResult icr = IntercoreSend(&icc, &hlAppId, txMsg, sizeof(txMsg) - 1);
-    if (icr != Intercore_OK) {
-        Uart_WriteStringPoll("IntercoreSend: ");
-        Uart_WriteIntegerPoll(icr);
-        Uart_WriteStringPoll("\r\n");
+    msg[msgLen - 2] = '0' + (msgCounter % 10);
+    msg[msgLen - 3] = '0' + (msgCounter / 10);
+
+    int32_t error = Socket_Write(socket, &A7ID, msg, msgLen);
+
+    if (error != ERROR_NONE) {
+        UART_Printf(debug, "ERROR: sending msg %s - %ld\r\n", msg, error);
+    }
+}
+
+static void handleSendMsgTimerWrapper(GPT *timer)
+{
+    (void)(timer);
+
+    static CallbackNode cbn = {.enqueued = false, .cb = handleSendMsgTimer, .data = NULL};
+    EnqueueCallback(&cbn);
+}
+
+static void handleRecvMsg(void *handle)
+{
+    Socket *socket = (Socket*)handle;
+
+    Component_Id senderId;
+    static char msg[32];
+    uint32_t msg_size = sizeof(msg);
+
+    int32_t error = Socket_Read(socket, &senderId, msg, &msg_size);
+
+    if (error != ERROR_NONE) {
+        UART_Printf(debug, "ERROR: receiving msg %s - %ld\r\n", msg, error);
     }
 
-    txMsg[txMsgLen - 3] = '0' + (iter / 10);
-    txMsg[txMsgLen - 2] = '0' + (iter % 10);
-    iter = (iter + 1) % 100;
-
-    MT3620_Gpt_LaunchTimerMs(TimerGpt0, sendTimerIntervalMs, HandleSendTimerIrq);
+    msg[msg_size] = '\0';
+    UART_Printf(debug, "Message received: %s\r\nSender: ", msg);
+    printComponentId(&senderId);
 }
 
-// Prints a sequence of bytes. If the start position occurs after the end
-// position, they are printed in reverse order. Therefore, this function can
-// print big- or little-endian values.
-static void PrintBytes(const void *buf, int start, int end)
+static void handleRecvMsgWrapper(Socket *handle)
 {
-    const uint8_t *buf8 = (const uint8_t *)buf;
-    int step = (end >= start) ? +1 : -1;
+    static CallbackNode cbn = {.enqueued = false, .cb = handleRecvMsg, .data = NULL};
 
-    for (/* nop */; start != end; start += step) {
-        Uart_WriteHexBytePoll(buf8[start]);
+    if (!cbn.data) {
+        cbn.data = handle;
     }
-    Uart_WriteHexBytePoll(buf8[end]);
+    EnqueueCallback(&cbn);
 }
 
-// Renders the supplied component ID as a string "00112233-4455-6677-8899-aabbccddeeff".
-// The first three values are interpreted as little-endian, and last two as big-endian.
-static void PrintGuid(const ComponentId *cid)
+// Button Callbacks
+// Enqueued when user presses A
+static void buttonA(void *data)
 {
-    PrintBytes(&cid->data1, 3, 0); // 4-byte little-endian word
-    Uart_WriteStringPoll("-");
-    PrintBytes(&cid->data2, 1, 0); // 2-byte little-endian half
-    Uart_WriteStringPoll("-");
-    PrintBytes(&cid->data3, 1, 0); // 2-byte little-endian half
-    Uart_WriteStringPoll("-");
-    PrintBytes(&cid->data4, 0, 1); // 2 bytes
-    Uart_WriteStringPoll("-");
-    PrintBytes(&cid->data4, 2, 7); // 6 bytes
+    (void)data;
+    msgCounter = (msgCounter + 1) % 100;
+    UART_Printf(debug, "Incrementing counter: %u\r\n", msgCounter);
 }
 
-// Runs with interrupts enabled. Retrieves messages from the inbound buffer
-// and prints their sender ID, length, and content (hex and text).
-static void HandleReceivedMessageDeferred(void)
+// Enqueued when user presses B
+static void buttonB(void *data)
 {
-    for (;;) {
-        ComponentId sender;
-        uint8_t rxData[32];
-        size_t rxDataSize = sizeof(rxData);
+    (void)data;
+    msgCounter = (msgCounter - 1) % 100;
+    UART_Printf(debug, "Decrementing counter: %u\r\n", msgCounter);
+}
 
-        IntercoreResult icr = IntercoreRecv(&icc, &sender, rxData, &rxDataSize);
+typedef struct ButtonState {
+    bool         prevState;
+    CallbackNode cbn;
+    uint32_t     gpioPin;
+} ButtonState;
 
-        // Return if read all messages in buffer.
-        if (icr == Intercore_Recv_NoBlockSize) {
-            return;
-        }
+static ButtonState buttons[NUM_BUTTONS] = {
+    {.prevState = true,
+     .cbn = {.enqueued = false, .cb = buttonA, .data = NULL},
+     .gpioPin   = 12},
+    {.prevState = true,
+     .cbn = {.enqueued = false, .cb = buttonB, .data = NULL},
+     .gpioPin   = 13}
+};
 
-        // Return if an error occurred.
-        if (icr != Intercore_OK) {
-            Uart_WriteStringPoll("IntercoreRecv: ");
-            Uart_WriteIntegerPoll(icr);
-            Uart_WriteStringPoll("\r\n");
-            return;
-        }
+static void handleButtonCallback(GPT *handle)
+{
+    (void)(handle);
+    // Assume initial state is high, i.e. button not pressed.
+    bool newState, pressed;
 
-        // Display sender component ID.
-        Uart_WriteStringPoll("Sender: ");
-        PrintGuid(&sender);
-        Uart_WriteStringPoll("\r\n");
-
-        Uart_WriteStringPoll("Message size: ");
-        Uart_WriteIntegerPoll((int)rxDataSize);
-        Uart_WriteStringPoll(" bytes:\r\n");
-
-        // Print message as hex.
-        Uart_WriteStringPoll("Hex: ");
-        for (uint32_t i = 0; i < rxDataSize; ++i) {
-            Uart_WriteHexBytePoll(rxData[i]);
-            if (i != rxDataSize - 1) {
-                Uart_WriteStringPoll(":");
+    for (unsigned i = 0; i < NUM_BUTTONS; i++) {
+        GPIO_Read(buttons[i].gpioPin, &newState);
+        if (newState != buttons[i].prevState) {
+            pressed = !newState;
+            if (pressed) {
+                EnqueueCallback(&buttons[i].cbn);
             }
         }
-        Uart_WriteStringPoll("\r\n");
-
-        // Print message as text.
-        Uart_WriteStringPoll("Text: ");
-        for (uint32_t i = 0; i < rxDataSize; ++i) {
-            char c[2];
-            c[0] = isprint(rxData[i]) ? rxData[i] : '.';
-            c[1] = '\0';
-            Uart_WriteStringPoll(c);
-        }
-        Uart_WriteStringPoll("\r\n");
+        buttons[i].prevState = newState;
     }
 }
 
-static _Noreturn void RTCoreMain(void)
+static CallbackNode *volatile callbacks = NULL;
+
+static void EnqueueCallback(CallbackNode *node)
 {
-    // The debugger will not connect until shortly after the application has started running.
-    // To use the debugger with code which runs at application startup, change the initial value
-    // of b from true to false, run the app, break into the app with a debugger, and set b to true.
-    volatile bool b = true;
-    while (!b) {
-        // empty.
+    uint32_t prevBasePri = NVIC_BlockIRQs();
+    if (!node->enqueued) {
+        CallbackNode *prevHead = callbacks;
+        node->enqueued = true;
+        callbacks = node;
+        node->next = prevHead;
+    }
+    NVIC_RestoreIRQs(prevBasePri);
+}
+
+static void InvokeCallbacks(void)
+{
+    CallbackNode *node;
+    do {
+        uint32_t prevBasePri = NVIC_BlockIRQs();
+        node = callbacks;
+        if (node) {
+            node->enqueued = false;
+            callbacks = node->next;
+        }
+        NVIC_RestoreIRQs(prevBasePri);
+
+        if (node) {
+            (node->cb)(node->data);
+        }
+    } while (node);
+}
+
+_Noreturn void RTCoreMain(void)
+{
+    VectorTableInit();
+    CPUFreq_Set(197600000);
+
+    debug = UART_Open(MT3620_UNIT_UART_DEBUG, 115200, UART_PARITY_NONE, 1, NULL);
+    UART_Print(debug, "--------------------------------\r\n");
+    UART_Print(debug, "IntercoreComms_MT3620_BareMetal\r\n");
+    UART_Print(debug, "App built on: " __DATE__ " " __TIME__ "\r\n");
+
+    // Initialise timers and supported speeds
+    for (unsigned i = 0; i < TIMER_COUNT; i++) {
+        timer[i] = GPT_Open(MT3620_UNIT_GPT0 + i, MT3620_GPT_012_HIGH_SPEED, GPT_MODE_REPEAT);
+        if (!timer[i]) {
+            UART_Printf(debug,
+                "ERROR: GPT%u initialisation failed\r\n", i);
+        }
     }
 
-    // SCB->VTOR = ExceptionVectorTable
-    WriteReg32(SCB_BASE, 0x08, (uint32_t)ExceptionVectorTable);
+    // Setup socket
+    socket = Socket_Open(handleRecvMsgWrapper);
+    if (!socket) {
+        UART_Printf(debug, "ERROR: socket initialisation failed\r\n");
+    }
 
-    Uart_Init();
-    Uart_WriteStringPoll("--------------------------------\r\n");
-    Uart_WriteStringPoll("IntercoreComms_RTApp_MT3620_BareMetal\r\n");
-    Uart_WriteStringPoll("App built on: " __DATE__ ", " __TIME__ "\r\n");
+    GPIO_ConfigurePinForInput(buttons[0].gpioPin);
+    GPIO_ConfigurePinForInput(buttons[1].gpioPin);
+    GPIO_ConfigurePinForOutput(gpioOut[0]);
+    GPIO_ConfigurePinForOutput(gpioOut[1]);
 
-    MT3620_Gpt_Init();
+    // Setup buttons
+    int32_t error;
+    if ((error = GPT_SetMode(timer[TIMER_BUTTONS], GPT_MODE_REPEAT)) != ERROR_NONE) {
+        UART_Printf(debug, "ERROR: Button GPT_SetMode failed %ld\r\n", error);
+    }
+    if ((error = GPT_StartTimeout(
+        timer[TIMER_BUTTONS], 100, GPT_UNITS_MILLISEC,
+        handleButtonCallback)) != ERROR_NONE) {
+        UART_Printf(debug, "ERROR: Button GPT_StartTimeout failed %ld\r\n", error);
+    }
 
-    IntercoreResult icr = SetupIntercoreComm(&icc, HandleReceivedMessageDeferred);
-    if (icr != Intercore_OK) {
-        Uart_WriteStringPoll("SetupIntercoreComm: ");
-        Uart_WriteIntegerPoll(icr);
-        Uart_WriteStringPoll("\r\n");
-    } else {
-        MT3620_Gpt_LaunchTimerMs(TimerGpt0, sendTimerIntervalMs, HandleSendTimerIrq);
+    // Setup Msg out
+    if ((error = GPT_SetMode(timer[TIMER_SEND_MSG], GPT_MODE_REPEAT)) != ERROR_NONE) {
+        UART_Printf(debug, "ERROR: Msg GPT_SetMode failed %ld\r\n", error);
+    }
+    if ((error = GPT_StartTimeout(
+        timer[TIMER_SEND_MSG], 1, GPT_UNITS_SECOND,
+        handleSendMsgTimerWrapper)) != ERROR_NONE) {
+        UART_Printf(debug, "ERROR: Msg GPT_StartTimeout failed %ld\r\n", error);
     }
 
     for (;;) {
-        InvokeDeferredProcs();
         __asm__("wfi");
+        InvokeCallbacks();
     }
 }
